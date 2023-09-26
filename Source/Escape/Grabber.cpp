40155fe22771e800bf33c5cf87518e5c4afda770
Tamás Gyetvai <34650749+gyetvaitamas@users.noreply.github.com>
2023-09-26 20:19:00 +0200

    Using FindComponentByClass & Key binding

--- Source/Escape/Grabber.cpp
+++ Source/Escape/Grabber.cpp
@@ -23,6 +23,33 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber!"));
+
+	// Look for attached PhysicsHandle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle)
+	{
+		// PhysicsHandle has found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s class is not found!"), *GetOwner()->GetName());
+	}
+
+	// Look for attached InputComponent (on run-time)
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (InputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Input component found!"));
+
+		// Bind the input axis
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s class is not found!"), *GetOwner()->GetName());
+	}
 	
 }
 
@@ -68,5 +95,10 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s."), *(ActorHit->GetName()));
 	}
 	
+}
+
+void UGrabber::Grab()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grabbed."));
 }
 
--- Source/Escape/Grabber.h
+++ Source/Escape/Grabber.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include <PhysicsEngine/PhysicsHandleComponent.h>
 #include "Grabber.generated.h"
 
 
@@ -25,7 +26,13 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
+	// How far ahead of the player can we reach in cm
 	float Reach = 100.0f;
 
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
 		
 };
